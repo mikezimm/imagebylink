14e07b129af259e66382d9e5bd3148451a6cb05a
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-10-31 20:46:04 -0400

    Initial Commit

---
+++ src/webparts/imagebylink/components/IImagebylinkProps.ts
@@ -0,0 +1,3 @@
+export interface IImagebylinkProps {
+  description: string;
+}
---
+++ src/webparts/imagebylink/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IImagebylinkWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ImagebylinkWebPartStrings' {
+  const strings: IImagebylinkWebPartStrings;
+  export = strings;
+}
